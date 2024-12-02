6734017502b3d6d4221f8607af1897f524287214
dejohansson <33880373+dejohansson@users.noreply.github.com>
2024-12-02 19:17:14 +0100

    Day 2: Part 2

--- day-02/main.ts
+++ day-02/main.ts
@@ -5,40 +5,32 @@ function reportIsSafe(report: number[]): boolean {
 
   for (let i = 0; i < report.length - 1; i++) {
     if (
-      increasing &&
-      (report[i + 1] - report[i] < 1 || report[i + 1] - report[i] > 3)
-    ) {
-      return false;
-    } else if (
-      !increasing &&
-      (report[i] - report[i + 1] < 1 || report[i] - report[i + 1] > 3)
+      increasing
+        ? report[i + 1] - report[i] < 1 || report[i + 1] - report[i] > 3
+        : report[i] - report[i + 1] < 1 || report[i] - report[i + 1] > 3
     ) {
       return false;
     }
   }
-  console.log(report)
   return true;
 }
 
 function reportIsSafeWithDampener(report: number[]): boolean {
-  if (report[0] === report[1]) console.log(report[0])
-  if (report[0] === report[1])
-    return (
-      reportIsSafe(report.toSpliced(0, 1)) ||
-      reportIsSafe(report.toSpliced(1, 1))
-    );
+  let increasing = undefined;
 
-  const increasing = report[0] < report[1];
+  if (report[0] < report[1] && report[1] < report[2]) increasing = true;
+  else if (report[0] > report[1] && report[1] > report[2]) increasing = false;
+  else return reportIsSafe(report.toSpliced(0, 1)) || reportIsSafe(report.toSpliced(1, 1));
 
   for (let i = 0; i < report.length - 1; i++) {
     if (
-      (increasing && (report[i + 1] - report[i] < 1 || report[i + 1] - report[i] > 3)) ||
-      (!increasing && (report[i] - report[i + 1] < 1 || report[i] - report[i + 1] > 3))
+      increasing
+        ? report[i + 1] - report[i] < 1 || report[i + 1] - report[i] > 3
+        : report[i] - report[i + 1] < 1 || report[i] - report[i + 1] > 3
     ) {
       return reportIsSafe(report.toSpliced(i, 1)) || reportIsSafe(report.toSpliced(i + 1, 1));
     }
   }
-  console.log(report)
   return true;
 }
 
@@ -53,14 +45,10 @@ function main() {
   console.log(`Number of safe reports: ${numSafeReports}`);
 
   const numSafeReportsWithDampener = input.split("\n").reduce((acc, line) => {
-    return (
-      acc + (reportIsSafeWithDampener(line.split(" ").map(Number)) ? 1 : 0)
-    );
+    return acc + (reportIsSafeWithDampener(line.split(" ").map(Number)) ? 1 : 0);
   }, 0);
 
-  console.log(
-    `Number of safe reports with dampener: ${numSafeReportsWithDampener}`
-  );
+  console.log(`Number of safe reports with dampener: ${numSafeReportsWithDampener}`);
 }
 
 main();
